74b9b0a6e84523a042e7dc7efed15a38d763e379
santiagoaluas <82826216+santiagoaluas@users.noreply.github.com>
2025-12-03 00:09:57 -0500

    sincronizacion deudas crecos activas

--- helpers/MarcarIncumplidosDailyWorker.cs
+++ helpers/MarcarIncumplidosDailyWorker.cs
@@ -56,15 +56,15 @@ namespace gestiones_backend.helpers
                     zipService.DescomprimirZipsUltimo();
                     _logger.LogInformation("Zips descomprimidos.");
 
-                    //await importSvc.ImportarDeudoresCompletoAsync();
-                    //await importSvc.ImportarTelefonosBasicoAsync();
-                    //importSvc.GrabarTablas();
-                    //importSvc.ImportarDeudas();
+                    await importSvc.ImportarDeudoresCompletoAsync();
+                    await importSvc.ImportarTelefonosBasicoAsync();
+                    importSvc.GrabarTablas();
+                    importSvc.ImportarDeudas();
                     //importSvc.importarPagos();
                     //_logger.LogInformation("Datos importados correctamente.");
 
-                   // var updated = await compromisos.MarcarIncumplidosVencidosAsync();
-                  //  _logger.LogInformation("Job 'MarcarIncumplidos' ejecutado. Filas actualizadas: {Updated}", updated);
+                    // var updated = await compromisos.MarcarIncumplidosVencidosAsync();
+                    //  _logger.LogInformation("Job 'MarcarIncumplidos' ejecutado. Filas actualizadas: {Updated}", updated);
                 }
                 catch (Exception ex)
                 {
